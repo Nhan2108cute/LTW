59f288eaaad06ddb825ab800f5c5af5f4a4afc8a
Nhan2108cute <21130460@st.hcmuaf.edu.vn>
2023-10-16 14:05:35 +0700

    LTW

---
+++ JAVA/Demo_Project/src/main/java/Hello.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("*/hello")
+public class Hello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("hisiiiiiiii");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("cc");
+    }
+}
---
+++ JAVA/Demo_Project/src/main/java/hehe.java
@@ -0,0 +1,8 @@
+import javax.servlet.http.HttpServlet;
+
+public class hehe extends HttpServlet {
+    public static void main(String[] args) {
+        System.out.println("CCC");
+    }
+
+}
